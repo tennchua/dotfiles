e5f6e992322dad67add4b1c2d6e8b36b9fa977cf
Jamie Alessio <jamie@stoic.net>
2013-10-07 10:26:01 -0700

    Add -f to ln command to suppress errors on re-run. Convert tabs to spaces.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 function doIt() {
-	dir=~/dotfiles # dotfiles directory
-	for file in $(git ls-files | grep -v README.md | grep -v bootstrap.sh | cut -d"/" -f1|uniq); do
-		if [[ ( -f "$file" ) || ( -d "$file" ) ]]; then
-	    	echo "Creating symlink to $file in home directory."
-	    	ln -s $dir/$file ~/$file
-	    fi
-	done
+  dir=~/dotfiles # dotfiles directory
+  for file in $(git ls-files | grep -v README.md | grep -v bootstrap.sh | cut -d"/" -f1|uniq); do
+    if [[ ( -f "$file" ) || ( -d "$file" ) ]]; then
+      echo "Creating symlink to $file in home directory."
+      ln -sf $dir/$file ~/$file
+    fi
+  done
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
 fi
 unset doIt
 # source ~/.zshrc # this throws errors, presumably because of the #!/bin/bash at the top of this file
