357b94bed31cdced0393ffae56dcabb07116ec97
Jamie Alessio <jamie@stoic.net>
2013-10-21 11:09:22 -0700

    Adding oh-my-zsh theme symlink.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,16 @@
 
 function doIt() {
   dir=~/dotfiles # dotfiles directory
-  for file in $(git ls-files | grep -v README.md | grep -v bootstrap.sh | cut -d"/" -f1|uniq); do
+  for file in $(git ls-files | grep -v README.md | grep -v bootstrap.sh | cut -d"/" -f1 | uniq); do
     if [[ ( -f "$file" ) || ( -d "$file" ) ]]; then
       echo "Creating symlink to $file in home directory."
       ln -sf $dir/$file ~/$file
     fi
+  done
+  # oh-my-zsh thems
+  for file in $(git ls-files .oh-my-zsh/themes | cut -d"/" -f3 | uniq); do
+    echo "Creating symlink for ~/.oh-my-zsh/themes/$file"
+    ln -sf $dir/.oh-my-zsh/themes/$file ~/.oh-my-zsh/themes/$file
   done
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
